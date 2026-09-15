f3f65ef3fb1b8d2ee4077e9b3f408edb1486ff61
Marcos Cleiton <marcoscleiton12@hotmail.com>
2026-09-15 11:10:44 -0300

    fix: middleware diferencia erros 400 (validação) de 500 (servidor)

--- src/controllers/transacoes.controllers.js
+++ src/controllers/transacoes.controllers.js
@@ -3,7 +3,8 @@ import pool from "../db/connection.js";
 const adicionarTransacao = async (req, res, next) => {
     try {
         const { descricao, valor, tipo, data, categoria_id} = req.body;
-
+        
+        
         const resultado = await pool.query(
             "INSERT INTO transacoes (descricao, valor, tipo, data, categoria_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
             [descricao, valor, tipo, data, categoria_id]
--- src/middlewares/tratamentoDeErros.js
+++ src/middlewares/tratamentoDeErros.js
@@ -2,7 +2,11 @@ import logger from "../utils/logger.js";
 
 function tratamentoDeErro(erro, req, res, next) {
     logger(erro);
-    res.status(500).json({ erro: "Erro ao tentar acessar o servidor" });
+    
+    const status = erro.status || 500;
+    const mensagem = status === 500 ? "Erro ao tentar acessar o servidor" : erro.message;
+
+    res.status(status).json({ erro: mensagem });
 }
 
 export default tratamentoDeErro;
